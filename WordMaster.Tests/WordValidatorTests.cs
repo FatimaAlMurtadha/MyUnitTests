c56998d153423acb4a5b6039b390f657943a53f6
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-03-25 11:52:14 +0100

    Tests long and empty words pass

--- WordMaster.Tests/WordValidatorTests.cs
+++ WordMaster.Tests/WordValidatorTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 public class WordValidateTests
 {
-  // 
+  // 1. less than 3 letters  ** FAILD **
   [Fact]
   public void Word_WithLessThanThreeLetters_ShouldBeInvalid()
   {
@@ -21,7 +21,7 @@ public class WordValidateTests
 
   // Here comes more rules
 
-  // 1. The word should also be valid
+  // 2. The word should also be valid 3 letters ** PASS **
 
   [Fact]
   public void Word_WithThreeLetters_ShouldBeValid()
@@ -34,6 +34,28 @@ public class WordValidateTests
 
     // Assert
     Assert.True(result);
+  }
+
+  // 3. A long word more than 3 letters ** PASS **
+  [Fact]
+  public void Word_WithManyLetters_ShouldBeValid()
+  {
+    WordValidator validator = new WordValidator();
+
+    bool result = validator.IsValidLength("elephant");
+
+    Assert.True(result);
+  }
+
+  // 4. an empty word ** FAILD **
+  [Fact]
+  public void EmptyWord_ShouldBeInvalid()
+  {
+    WordValidator validator = new WordValidator();
+
+    bool result = validator.IsValidLength("");
+
+    Assert.False(result);
   }
 }
 
