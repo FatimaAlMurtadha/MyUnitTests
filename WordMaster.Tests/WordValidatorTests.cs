71dd8ac954c2693f221f0181317db0e5e43cc6bf
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-03-25 12:05:32 +0100

    create The second feature - Score system

--- WordMaster.Tests/WordValidatorTests.cs
+++ WordMaster.Tests/WordValidatorTests.cs
@@ -1,10 +1,15 @@
-namespace WordMaster.WordMaster.Tests;
+namespace WordMaster.Tests;
 
 using global::WordMaster.Domain;
 using Xunit;
 
 public class WordValidateTests
 {
+  
+  /*
+   * The first feature  // the word length
+   * */
+   
   // 1. less than 3 letters  ** FAILD **
   [Fact]
   public void Word_WithLessThanThreeLetters_ShouldBeInvalid()
@@ -56,6 +61,21 @@ public class WordValidateTests
     bool result = validator.IsValidLength("");
 
     Assert.False(result);
+  }
+  
+  /*
+   * The second feature // Score system
+   * // 1 point for every letter and Bonus for words long words +5 
+   */
+
+  // 1. a 3 letters word
+  [Fact]
+  public void ShortWord_ShouldReturnLengthScore()
+  {
+    ScoreCalculator calculator = new ScoreCalculator();
+    int score = calculator.CalculateScore("cat");
+    
+    Assert.Equal(3, score);
   }
 }
 
