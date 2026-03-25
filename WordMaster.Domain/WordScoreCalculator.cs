33e701331203f2f4579e44a6a2a9f8f3b63908c3
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-03-25 14:08:30 +0100

    managed spaces, 0 and null

--- WordMaster.Domain/WordScoreCalculator.cs
+++ WordMaster.Domain/WordScoreCalculator.cs
@@ -4,15 +4,27 @@ public class WordScoreCalculator
 {
     public int CalculateScore(string word)
     {
+
+        if (word == null)
+        {
+            throw new ArgumentNullException(nameof(word));
+        }
+
+        word = word.Trim();
+        
+        if (word.Length == 0)
+        {
+            return 0;
+        }
+
         int score = word.Length;
 
         if (word.Length > 5)
         {
             score += 5;
-            
+
         }
 
         return score;
-        
     }
 }
