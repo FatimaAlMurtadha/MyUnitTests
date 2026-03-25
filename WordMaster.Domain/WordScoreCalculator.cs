4eb8658dcbbfce2c6cb43ec179be5d62739b84d4
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-03-25 12:16:13 +0100

    Added score calculator function

---
+++ WordMaster.Domain/WordScoreCalculator.cs
@@ -0,0 +1,16 @@
+namespace WordMaster.Domain;
+
+public class WordScoreCalculator
+{
+    public int CalculateScore(string word)
+    {
+        int score = word.Length;
+
+        if (word.Length <5)
+        {
+            score += 5;
+            return score;
+        }
+        
+    }
+}
